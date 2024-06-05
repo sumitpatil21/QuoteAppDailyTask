5363b26e28ce743164cf9c88e2f1fead2eefa657
SUMIT <sumitspatil205@gmail.com>
2024-06-05 22:56:25 +0530

    5/6/24

--- lib/Listview_to_Gridview.dart
+++ lib/Listview_to_Gridview.dart
@@ -79,16 +79,22 @@ class _ListviewtoGridviewState extends State<ListviewtoGridview> {
 
 
 
-  GridView buildGridView() {
-    return GridView.builder(
-      itemCount: quotesvar!.quotesList.length,
-      gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemBuilder: (context, index) => Card(
-        color: colorArray[index],
-        child: ListTile(
-          title: Text(quotesvar!.quotesList[index].quotesa!,style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),),
-          subtitle: Text(quotesvar!.quotesList[index].author!,style: TextStyle(fontWeight: FontWeight.w700),),
+  Scrollbar buildGridView() {
+    return Scrollbar(
+interactive: true,
+      thickness: 10,
+      radius: Radius.circular(10),
+      thumbVisibility: true,
+      child: GridView.builder(
+        itemCount: quotesvar!.quotesList.length,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (context, index) => Card(
+          color: colorArray[index],
+          child: ListTile(
+            title: Text(quotesvar!.quotesList[index].quotesa!,style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),),
+            subtitle: Text(quotesvar!.quotesList[index].author!,style: TextStyle(fontWeight: FontWeight.w700),),
+          ),
         ),
       ),
     );
@@ -130,5 +136,23 @@ List<Color> colorArray = [
   Color(0xFF999933), Color(0xFFFF3380), Color(0xFFCCCC00),
   Color(0xFF66E64D), Color(0xFF4D80CC), Color(0xFF9900B3),
   Color(0xFFE64D66), Color(0xFF4DB380), Color(0xFFFF4D4D),
-  Color(0xFF99E6E6), Color(0xFF6666FF)
+  Color(0xFF99E6E6), Color(0xFF6666FF), Color(0xFFFF6633), Color(0xFFFFB399), Color(0xFFFF33FF),
+  Color(0xFFFFFF99), Color(0xFF00B3E6), Color(0xFFE6B333),
+  Color(0xFF3366E6), Color(0xFF999966), Color(0xFF99FF99),
+  Color(0xFFB34D4D), Color(0xFF80B300), Color(0xFF809900),
+  Color(0xFFE6B3B3), Color(0xFF6680B3), Color(0xFF66991A),
+  Color(0xFFFF99E6), Color(0xFFCCFF1A), Color(0xFFFF1A66),
+  Color(0xFFE6331A), Color(0xFF33FFCC), Color(0xFF66994D),
+  Color(0xFFB366CC), Color(0xFF4D8000), Color(0xFFB33300),
+  Color(0xFFCC80CC), Color(0xFF66664D), Color(0xFF991AFF),
+  Color(0xFFE666FF), Color(0xFF4DB3FF), Color(0xFFFF6633), Color(0xFFFFB399), Color(0xFFFF33FF),
+  Color(0xFFFFFF99), Color(0xFF00B3E6), Color(0xFFE6B333),
+  Color(0xFF3366E6), Color(0xFF999966), Color(0xFF99FF99),
+  Color(0xFFB34D4D), Color(0xFF80B300), Color(0xFF809900),
+  Color(0xFFE6B3B3), Color(0xFF6680B3), Color(0xFF66991A),
+  Color(0xFFFF99E6), Color(0xFFCCFF1A), Color(0xFFFF1A66),
+  Color(0xFFE6331A), Color(0xFF33FFCC), Color(0xFF66994D),
+  Color(0xFFB366CC), Color(0xFF4D8000), Color(0xFFB33300),
+  Color(0xFFCC80CC), Color(0xFF66664D), Color(0xFF991AFF),
+  Color(0xFFE666FF), Color(0xFF4DB3FF)
 ];
